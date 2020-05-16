9cdb1a3d4b1235931394ea4cf8e6cfd2b1775b80
Vlad <vladislav.vnb2020@gmail>
2020-05-16 15:02:21 +0300

    Тестовое задание для стажировки JavaRush

---
+++ src/main/java/com/space/controller/ShipController.java
@@ -0,0 +1,92 @@
+package com.space.controller;
+
+
+import com.space.model.Ship;
+import com.space.model.ShipType;
+import com.space.service.ShipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@ResponseBody
+@RequestMapping(value = "/rest")
+public class ShipController {
+
+    private final ShipService shipService;
+
+    @Autowired
+    public ShipController(ShipService shipService) {
+        this.shipService = shipService;
+    }
+
+    @RequestMapping(value = "/ships",method = RequestMethod.GET)
+    public ResponseEntity<List<Ship>> getShipList( String name,String planet,ShipType shipType,Long after,Long before,
+                                   Boolean isUsed,Double minSpeed,Double maxSpeed,Integer minCrewSize,
+                                   Integer maxCrewSize,Double minRating,Double maxRating,
+                                  @RequestParam(required = false) ShipOrder order,
+                                  @RequestParam(required = false) Integer pageNumber,
+                                  @RequestParam(required = false) Integer pageSize) {
+        List<Ship> filteredShips = shipService.getShipList(name, planet, shipType, after, before, isUsed,
+                                                           minSpeed,maxSpeed, minCrewSize, maxCrewSize,
+                                                           minRating, maxRating);
+        return new ResponseEntity<>(shipService.filteredShips(filteredShips, order, pageNumber, pageSize),HttpStatus.OK);
+     }
+
+    @RequestMapping(value = "/ships/{id}",method = RequestMethod.GET)
+    public ResponseEntity<Ship> getShipById(@PathVariable Long id) {
+         if(id == null && id < 0 &&  id - Math.floor(id) !=0){
+             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+         } else
+         if(shipService.getShipById(id) == null) {
+             return new ResponseEntity<>(shipService.getShipById(id),HttpStatus.NOT_FOUND);
+         }
+         return new ResponseEntity<>(shipService.getShipById(id),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/ships",method = RequestMethod.POST)
+    public ResponseEntity<Ship> createShip(@RequestBody Ship ship) {
+        Ship createShip = shipService.createShip(ship);
+        if (createShip == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(createShip,HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/ships/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Ship> deleteShip(@PathVariable Long id) {
+        if(id == null && id < 0 && id - Math.floor(id) !=0){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else
+        if(shipService.getShipById(id) == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        shipService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/ships/{id}",method = RequestMethod.POST)
+    public ResponseEntity<Ship> updateShip(@RequestBody Ship ship, @PathVariable Long id) {
+        if(id != null && id > 0 &&  id - Math.floor(id) !=0 ){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }else
+        if(shipService.getShipById(id) == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(shipService.updateShip(ship, id),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/ships/count",method = RequestMethod.GET)
+    public ResponseEntity<Integer>  getShipCount(String name,String planet,ShipType shipType,Long after, Long before,
+                            Boolean isUsed,Double minSpeed,Double maxSpeed,Integer minCrewSize,
+                            Integer maxCrewSize, Double minRating, Double maxRating) {
+        int count = shipService.getShipList(name, planet, shipType, after, before, isUsed, minSpeed,
+                maxSpeed, minCrewSize,maxCrewSize, minRating, maxRating).size();
+        return new ResponseEntity<>(count,HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/space/service/ShipService.java
@@ -0,0 +1,197 @@
+package com.space.service;
+
+import com.space.controller.ShipOrder;
+import com.space.model.Ship;
+import com.space.model.ShipType;
+import com.space.repository.ShipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ShipService {
+
+    private final ShipRepository shipRepository;
+
+    @Autowired
+    public ShipService(ShipRepository shipRepository) {
+        this.shipRepository = shipRepository;
+    }
+
+    @Transactional
+    public Ship createShip(Ship ship) {
+        if (ship.getName() == null ||
+                ship.getName().isEmpty() ||
+                ship.getName().length() > 50 ||
+                ship.getPlanet() == null ||
+                ship.getPlanet().isEmpty() ||
+                ship.getPlanet().length() > 50 ||
+                ship.getShipType() == null ||
+                ship.getProdDate() == null ||
+                ship.getProdDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear() < 2800 ||
+                ship.getProdDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear() > 3019 ||
+                ship.getSpeed() == null ||
+                ship.getSpeed() < 0.01d ||
+                ship.getSpeed() > 0.99d ||
+                ship.getCrewSize() == null ||
+                ship.getCrewSize() < 1 ||
+                ship.getCrewSize() > 9999) {
+            return null;
+        } else if (ship.getUsed() == null) {
+            ship.setUsed(false);
+        }
+
+        ship.setSpeed((double) Math.round(ship.getSpeed() * 100) / 100);
+        ship.setRating(countedShipRating(ship));
+        return shipRepository.save(ship);
+    }
+    @Transactional
+    public void delete(Long id) {
+        if (shipRepository.existsById(id)) {
+            shipRepository.deleteById(id);
+         }
+    }
+    @Transactional
+    public Ship getShipById(Long id) {
+        if (shipRepository.existsById(id)) {
+            return shipRepository.findById(id).orElse(null);
+        }
+        return null;
+    }
+
+    @Transactional
+    public Ship updateShip(Ship newShip, Long id) {
+        Ship shipUpdate = getShipById(id);
+
+        if (newShip == null || shipUpdate == null) {
+            return null;
+        }
+        if (newShip.getName() != null) {
+            if (newShip.getName().length() > 50 ||  newShip.getName().isEmpty()) {
+                return null;
+            }
+            shipUpdate.setName(newShip.getName());
+        }
+        if (newShip.getPlanet() != null) {
+            if (newShip.getPlanet().length() > 50 ||  newShip.getPlanet().isEmpty()) {
+                return null;
+            }
+            shipUpdate.setPlanet(newShip.getPlanet());
+        }
+        if (newShip.getShipType() != null) {
+            shipUpdate.setShipType(newShip.getShipType());
+        }
+        if (newShip.getProdDate() != null) {
+            if (newShip.getProdDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear() < 2800 ||
+                    newShip.getProdDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear() > 3019) {
+                return null;
+            }
+            shipUpdate.setProdDate(newShip.getProdDate());
+        }
+        if (newShip.getUsed() != null) {
+            shipUpdate.setUsed(newShip.getUsed());
+        }
+        if (newShip.getSpeed() != null) {
+            if ( newShip.getSpeed() < 0.01d ||  newShip.getSpeed() > 0.99d) {
+                return null;
+            }
+            shipUpdate.setSpeed(newShip.getSpeed());
+        }
+        if (newShip.getCrewSize() != null) {
+            if (newShip.getCrewSize() < 1 ||  newShip.getCrewSize() > 9999) {
+                return null;
+            }
+            shipUpdate.setCrewSize(newShip.getCrewSize());
+        }
+
+        shipUpdate.setRating(countedShipRating(shipUpdate));
+        return shipRepository.save(shipUpdate);
+    }
+
+    public List<Ship> getShipList(String name, String planet, ShipType shipType, Long after, Long before,
+                                  Boolean getUsed, Double minSpeed, Double maxSpeed, Integer minCrewSize,
+                                  Integer maxCrewSize, Double minRating, Double maxRating) {
+        List<Ship> ListShips = (List<Ship>) shipRepository.findAll();
+        if (name != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getName().contains(name)).collect(Collectors.toList());
+        }
+        if (planet != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getPlanet().contains(planet)).collect(Collectors.toList());
+        }
+        if (shipType != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getShipType().equals(shipType)).collect(Collectors.toList());
+        }
+        if (after != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getProdDate().after(new Date(after))).collect(Collectors.toList());
+        }
+        if (before != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getProdDate().before(new Date(before))).collect(Collectors.toList());
+        }
+        if (getUsed != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getUsed().equals(getUsed)).collect(Collectors.toList());
+        }
+        if (minSpeed != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getSpeed() >= minSpeed).collect(Collectors.toList());
+        }
+        if (maxSpeed != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getSpeed() <= maxSpeed).collect(Collectors.toList());
+        }
+        if (minCrewSize != null) {
+            ListShips = ListShips.stream()
+                    .filter(ship -> ship.getCrewSize() >= minCrewSize).collect(Collectors.toList());
+        }
+        if (maxCrewSize != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getCrewSize() <= maxCrewSize).collect(Collectors.toList());
+        }
+        if (minRating != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getRating() >= minRating).collect(Collectors.toList());
+        }
+        if (maxRating != null) {
+            ListShips = ListShips.stream().filter(ship -> ship.getRating() <= maxRating).collect(Collectors.toList());
+        }
+        return ListShips;
+
+    }
+
+    public List<Ship> filteredShips(final List<Ship> shipList, ShipOrder order, Integer pageNumber, Integer pageSize) {
+        pageNumber = pageNumber == null ? 0 : pageNumber;
+        pageSize = pageSize == null ? 3 : pageSize;
+        return shipList.stream().sorted(getComparator(order)).skip(pageNumber * pageSize).limit(pageSize).collect(Collectors.toList());
+    }
+
+    private Comparator<Ship> getComparator(ShipOrder shipOrder) {
+        if (shipOrder == null) {
+            return Comparator.comparing(Ship::getId);
+        }
+        Comparator<Ship> comparator = null;
+        switch (shipOrder.getFieldName()) {
+            case "id":
+                comparator = Comparator.comparing(Ship::getId);
+                break;
+            case "speed":
+                comparator = Comparator.comparing(Ship::getSpeed);
+                break;
+            case "prodDate":
+                comparator = Comparator.comparing(Ship::getProdDate);
+                break;
+            case "rating":
+                comparator = Comparator.comparing(Ship::getRating);
+        }
+
+        return comparator;
+    }
+
+        private Double countedShipRating(Ship ship) {
+        double speed = ship.getSpeed();
+        double coefficientUsed = ship.getUsed() ? 0.5d : 1.0d;
+        int currentYear = 3019;
+        int productionDate = ship.getProdDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear();
+        double rating = (80 * speed * coefficientUsed) / (double) (currentYear - productionDate + 1);
+        return (double) Math.round(rating * 100) / 100;
+    }
+}
